094806da0bc53db9b072888b448ebe99c0a57caa
Fe <Fe@Fe-ui-MacBook-Pro-4.local>
2018-08-31 17:41:24 +0900

    Commit from React Studio 1.5.3

--- Documents/React Studio/Works In Progress/My App - Untitled/src/StartScreen.js
+++ Documents/React Studio/Works In Progress/My App - Untitled/src/StartScreen.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import img_state0_elReactstudio_layoutdemo_headerpic from './images/StartScreen_state0_elReactstudio_layoutdemo_headerpic_360944.jpg';
-import img_state0_elImage624414 from './images/StartScreen_state0_elImage624414_624414.jpg';
-import img_state1_elImage624414 from './images/StartScreen_state0_elImage624414_624414.jpg';
-import img_state2_elImage624414 from './images/StartScreen_state0_elImage624414_624414.jpg';
+import img_elReactstudio_layoutdemo_headerpic from './images/StartScreen_elReactstudio_layoutdemo_headerpic_360944.jpg';
+import img_elImage from './images/StartScreen_elImage_624414.jpg';
 
 // UI framework component imports
 import Container from 'muicss/lib/react/container';
@@ -14,9 +12,7 @@ export default class StartScreen extends Component {
   // Properties used by this component:
   // appActions, deviceInfo
 
-  // --- Functions for component state index 0 (1 of 3) --- 
-  
-  renderState0() {
+  render() {
     // eslint-disable-next-line no-unused-vars
     let baseStyle = {};
     // eslint-disable-next-line no-unused-vars
@@ -41,18 +37,27 @@ export default class StartScreen extends Component {
         height: 'auto',
         pointerEvents: 'none',
      };
+    const style_card = {
+        width: '100%',
+        height: '100%',
+     };
+    const style_card_outer = {
+        backgroundColor: 'white',
+        boxShadow: '0.0px 5.3px 37px rgba(0, 0, 0, 0.4500)',
+        pointerEvents: 'none',
+     };
     const style_image = {
-        backgroundImage: 'url('+img_state0_elImage624414+')',
+        backgroundImage: 'url('+img_elImage+')',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: '50% 50%',
-        backgroundSize: 'cover',
+        backgroundSize: 'contain',
         pointerEvents: 'none',
      };
     
     return (
       <Container fluid={true} className="AppScreen StartScreen" style={baseStyle}>
         <div className="background">
-          <div className='appBg containerMinHeight state0_elBackground' style={style_background_outer}>
+          <div className='appBg containerMinHeight elBackground' style={style_background_outer}>
             <div style={style_background} />
           
           </div>
@@ -60,93 +65,17 @@ export default class StartScreen extends Component {
         </div>
         <div className="screenFgContainer">
           <div className="foreground">
-            <img className='containerMinHeight state0_elReactstudio_layoutdemo_headerpic' style={style_reactstudio_layoutdemo_headerpic} src={img_state0_elReactstudio_layoutdemo_headerpic} alt=""  />
-            <div className='state0_elImage624414' style={style_image} />
+            <img className='containerMinHeight elReactstudio_layoutdemo_headerpic' style={style_reactstudio_layoutdemo_headerpic} src={img_elReactstudio_layoutdemo_headerpic} alt=""  />
+            <div className='cardBg elCard' style={style_card_outer}>
+              <div style={style_card} />
+            
+            </div>
+            
+            <div className='elImage' style={style_image} />
           </div>
         </div>
       </Container>
     )
-  }
-  
-  // --- Functions for component state index 1 (2 of 3) --- 
-  
-  renderState1() {
-    // eslint-disable-next-line no-unused-vars
-    let baseStyle = {};
-    // eslint-disable-next-line no-unused-vars
-    let layoutFlowStyle = {};
-    if (this.props.transitionId && this.props.transitionId.length > 0 && this.props.atTopOfScreenStack && this.props.transitionForward) {
-      baseStyle.animation = '0.25s ease-in-out '+this.props.transitionId;
-    }
-    if ( !this.props.atTopOfScreenStack) {
-      layoutFlowStyle.height = '100vh';
-      layoutFlowStyle.overflow = 'hidden';
-    }
-    
-    const style_image = {
-        backgroundImage: 'url('+img_state1_elImage624414+')',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '50% 50%',
-        backgroundSize: 'cover',
-        pointerEvents: 'none',
-     };
-    
-    return (
-      <Container fluid={true} className="AppScreen StartScreen" style={baseStyle}>
-        <div className="screenFgContainer">
-          <div className="foreground">
-            <div className='state1_elImage624414' style={style_image} />
-          </div>
-        </div>
-      </Container>
-    )
-  }
-  
-  // --- Functions for component state index 2 (3 of 3) --- 
-  
-  renderState2() {
-    // eslint-disable-next-line no-unused-vars
-    let baseStyle = {};
-    // eslint-disable-next-line no-unused-vars
-    let layoutFlowStyle = {};
-    if (this.props.transitionId && this.props.transitionId.length > 0 && this.props.atTopOfScreenStack && this.props.transitionForward) {
-      baseStyle.animation = '0.25s ease-in-out '+this.props.transitionId;
-    }
-    if ( !this.props.atTopOfScreenStack) {
-      layoutFlowStyle.height = '100vh';
-      layoutFlowStyle.overflow = 'hidden';
-    }
-    
-    const style_image = {
-        backgroundImage: 'url('+img_state2_elImage624414+')',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '50% 50%',
-        backgroundSize: 'cover',
-        pointerEvents: 'none',
-     };
-    
-    return (
-      <Container fluid={true} className="AppScreen StartScreen" style={baseStyle}>
-        <div className="screenFgContainer">
-          <div className="foreground">
-            <div className='state2_elImage624414' style={style_image} />
-          </div>
-        </div>
-      </Container>
-    )
-  }
-  
-  
-  render() {
-    switch (parseInt((this.state && this.state.visualStateIndexOverride !== undefined) ? this.state.visualStateIndexOverride : this.props.visualStateIndex, 10)) {
-      default:
-      case 0:
-        return this.renderState0();
-      case 1:
-        return this.renderState1();
-      case 2:
-        return this.renderState2();
-    }
   }
   
 
