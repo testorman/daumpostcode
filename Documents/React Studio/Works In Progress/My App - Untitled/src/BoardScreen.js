3e5af027089327228e46acb0bfae3cce0cbd63dd
Fe <Fe@Fe-ui-MacBook-Pro-4.local>
2018-09-04 18:57:44 +0900

    Commit from React Studio 1.5.4

--- Documents/React Studio/Works In Progress/My App - Untitled/src/BoardScreen.js
+++ Documents/React Studio/Works In Progress/My App - Untitled/src/BoardScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Grid from './Grid';
 import Component1 from './Component1';
-import btn_icon_342475 from './images/btn_icon_342475.png';
+import btn_icon_347788 from './images/btn_icon_347788.png';
 
 // UI framework component imports
 import Appbar from 'muicss/lib/react/appbar';
@@ -13,9 +13,7 @@ export default class BoardScreen extends Component {
   // Properties used by this component:
   // appActions, deviceInfo
 
-  // --- Functions for component state index 0 (1 of 2) --- 
-  
-  renderState0() {
+  render() {
     // eslint-disable-next-line no-unused-vars
     let baseStyle = {};
     // eslint-disable-next-line no-unused-vars
@@ -51,7 +49,7 @@ export default class BoardScreen extends Component {
      };
     const style_iconButton = {
         display: 'block',
-        backgroundImage: 'url('+btn_icon_342475+')',
+        backgroundImage: 'url('+btn_icon_347788+')',
         backgroundRepeat: 'no-repeat',
         backgroundSize: '89.562%',
         backgroundPosition: '50% 0%',
@@ -65,14 +63,14 @@ export default class BoardScreen extends Component {
     return (
       <div className="AppScreen BoardScreen" style={baseStyle}>
         <div className="background">
-          <div className='appBg containerMinHeight state0_elBackground' style={style_background_outer}>
+          <div className='appBg containerMinHeight elBackground' style={style_background_outer}>
             <div style={style_background} />
           
           </div>
           
         </div>
         <div className="layoutFlow" style={layoutFlowStyle}>
-          <div className='hasNestedComps state0_elList'>
+          <div className='hasNestedComps elList'>
             <div style={style_list}>
               {items_list.map((row, index) => {
                 let itemClasses = `gridItem cols1_${index % 1} cols3_${index % 3} cols5_${index % 5} cols4_${index % 4}`;
@@ -94,60 +92,15 @@ export default class BoardScreen extends Component {
         
         <div className="screenFgContainer">
           <div className="foreground">
-            <div className='state0_elRectangle' style={style_rectangle} />
-            <div className='hasNestedComps state0_elComponent425276'>
+            <div className='elRectangle' style={style_rectangle} />
+            <div className='hasNestedComps elComponent'>
               <Component1 appActions={this.props.appActions} deviceInfo={this.props.deviceInfo} locStrings={this.props.locStrings} />
             </div>
-            <button className='actionFont state0_elIconButton' style={style_iconButton}   />
+            <button className='actionFont elIconButton' style={style_iconButton}   />
           </div>
         </div>
       </div>
     )
-  }
-  
-  // --- Functions for component state index 1 (2 of 2) --- 
-  
-  renderState1() {
-    // eslint-disable-next-line no-unused-vars
-    let baseStyle = {};
-    // eslint-disable-next-line no-unused-vars
-    let layoutFlowStyle = {};
-    if (this.props.transitionId && this.props.transitionId.length > 0 && this.props.atTopOfScreenStack && this.props.transitionForward) {
-      baseStyle.animation = '0.25s ease-in-out '+this.props.transitionId;
-    }
-    if ( !this.props.atTopOfScreenStack) {
-      layoutFlowStyle.height = '100vh';
-      layoutFlowStyle.overflow = 'hidden';
-    }
-    
-    const dataSheet_postfirebase = this.props.dataSheets['postfirebase'];
-    
-    return (
-      <div className="AppScreen BoardScreen" style={baseStyle}>
-        <Appbar className="navBar">
-          <div className="title">Board</div>  <div className="backBtn" onClick={ (ev)=>{ this.props.appActions.goBack() } }></div>
-        </Appbar>
-        
-        <div className="screenFgContainer">
-          <div className="foreground">
-            <div className='hasNestedComps state1_elComponent425276'>
-              <Component1 appActions={this.props.appActions} deviceInfo={this.props.deviceInfo} locStrings={this.props.locStrings} />
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-  
-  
-  render() {
-    switch (parseInt((this.state && this.state.visualStateIndexOverride !== undefined) ? this.state.visualStateIndexOverride : this.props.visualStateIndex, 10)) {
-      default:
-      case 0:
-        return this.renderState0();
-      case 1:
-        return this.renderState1();
-    }
   }
   
 
